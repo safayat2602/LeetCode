cb9d628692777b2dc27aa647660e73f91e5ccca9
safayat2602 <149889157+safayat2602@users.noreply.github.com>
2025-06-26 10:49:23 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (61.94%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n=grid.length;
+       int hasharr[]=new int[n*n+1];
+       for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<n;j++)
+        {
+            hasharr[grid[i][j]]++;
+        }
+       }
+       int repeat=-1;
+       int missing=-1;
+       for(int i=1;i<=hasharr.length;i++)
+       {
+         if(hasharr[i]>1)
+           repeat=i;
+         else if(hasharr[i]==0)
+           missing=i;
+         if(repeat!=-1 && missing!=-1)
+           break;
+       }
+       return new int[]{repeat,missing};
+    }
+}
